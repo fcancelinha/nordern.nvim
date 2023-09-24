8f775cffceae7bb9ac74c3747312fa1132c4c804
Filipe Cancelinha <fcancelinha@pm.me>
2023-09-24 19:40:17 +0100

    fix: attemp

--- lua/northern/init.lua
+++ lua/northern/init.lua
@@ -6,15 +6,15 @@
 -- License: MIT
 -- Year: 2023
 -- ********************************************************************************
-local northern = {}
+local Northern = {}
 
 local b = require("northern.base")
 local l = require("northern.lang")
 local p = require("northern.plugs")
 local u = require("northern.utils")
 
-function northern.load()
+function Northern.load()
 	u.load(b, l, p)
 end
 
-return northern
+return Northern
