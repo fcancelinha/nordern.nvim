a5ba8395b43752118c8dd26454a7e39ae0b20b12
Filipe Cancelinha <fcancelinha@pm.me>
2023-09-24 20:51:23 +0100

    fix: attempt

--- colors/northern.lua
+++ colors/northern.lua
@@ -1,13 +1,12 @@
 -- *******************************************************************************
--- Name: Northern.nvim
--- Description: Carefully crafted nord colorscheme for neovim in Lua, using arctic studio's palette choosing the warmer colors.
--- Author: @fcancelinha https://github.com/fcancelinha
--- Website: https://github.com/fcancelinha/northern.nvim
--- License: MIT
--- Year: 2023
+-- Name        : Northern.nvim
+-- Description : Carefully crafted nord colorscheme for neovim in Lua, using arctic studio's palette choosing the warmer colors.
+-- Author      : @fcancelinha https://github.com/fcancelinha
+-- Website     : https://github.com/fcancelinha/northern.nvim
+-- License     : MIT
+-- Year        : 2023
 -- ********************************************************************************
 
-package.loaded['northern'] = nil
 package.loaded['northern.base'] = nil
 package.loaded['northern.colors'] = nil
 package.loaded['northern.config'] = nil
