07000aa9c6a455afc7579fedfbf10d170228fd37
Filipe Cancelinha <filipe.cancelinha@siemens.com>
2024-08-13 13:55:25 +0100

    fix: nil pointer opts

--- lua/nordern/config.lua
+++ lua/nordern/config.lua
@@ -18,16 +18,18 @@ local function apply_transparent_background(gui)
 end
 
 function C.apply_opts(highlights, opts)
-    if opts.italicized_comments then
-        apply_italicized_comments(highlights.syntax)
-    end
+    if opts then
+        if opts.italicized_comments then
+            apply_italicized_comments(highlights.syntax)
+        end
 
-    if opts.brighter_comments then
-        apply_brighter_comments(highlights.syntax)
-    end
+        if opts.brighter_comments then
+            apply_brighter_comments(highlights.syntax)
+        end
 
-    if opts.transparent_background then
-        apply_transparent_background(highlights.gui)
+        if opts.transparent_background then
+            apply_transparent_background(highlights.gui)
+        end
     end
 end
 
